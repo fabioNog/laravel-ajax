8ef94ee57e6c51d32146bb0dddcc23eded3a8443
fabioNog <fabio.carmo@computacao.ufla.br>
2020-05-03 20:36:14 -0300

    Alterndo app.blade com o jquery para aceitar funções getJson

--- resources/views/pages/produtos/produtos.blade.php
+++ resources/views/pages/produtos/produtos.blade.php
@@ -77,5 +77,22 @@
         $('#categoriaProduto').val('');
         $('#dlgProdutos').modal('show')
     }
+
+    function carregarCategorias(){
+        $.getJSON('/api/categorias',function(data){        
+
+            for(i=0;i<data.length;i++){
+                opcao = '<option value = "' 
+                    + data[i].id + '">' 
+                    + data[i].nome 
+                    + '</option>';
+                $('#categoriaProduto').append(opcao);                    
+            }
+        });
+    }
+
+    $(function(){
+        carregarCategorias();
+    })
 </script>
 @endsection
