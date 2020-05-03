ad8c1c489bcba960b82376bdd045ae23e1f8d9c1
fabioNog <fabio.carmo@computacao.ufla.br>
2020-05-03 09:23:34 -0300

    Criando views pages/categorias pages/produtos

---
+++ resources/views/pages/categorias/editarCategoria.blade.php
@@ -0,0 +1,23 @@
+@extends('pages.app', ["current" => "categorias"])
+
+@section('body')
+<div class="card boder">
+    <div class="card-body">
+        <form action="/register/public/categorias/{{$cat->id}}" method="POST">
+            @csrf
+            <div class="form-group">
+                <label for="nomeCategoria">Nome da Categoria</label>
+                <input 
+                    type="text" 
+                    name="nomeCategoria" 
+                    id="nomeCategoria"
+                    value="{{$cat->nome}}"
+                    placeholder="Categoria"
+                >                
+            </div>
+            <button type="submit" class="btn btn-dark btn-sm">Editar</button>
+            <button type="cancel" class="btn btn-danger btn-sm">Cancelar</button>
+        </form>
+    </div>
+</div>    
+@endsection
