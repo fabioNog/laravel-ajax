e0cffe36b3c78f77723c6b2b0edab15ced562e51
fabioNog <fabio.carmo@computacao.ufla.br>
2020-05-03 20:37:08 -0300

    Adicionando novas rotas para a api e para produtos

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProdutosController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -8,3 +9,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::get('/categorias','CategoriasController@indexJson');
+Route::resource('/produtos','ProdutosController');
